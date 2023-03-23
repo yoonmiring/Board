83aa39ff31efa62cabdc02b04d0beeb21e85d4cc
yoonmiring <ymjeong@unours.co.kr>
2023-03-23 13:44:09 +0900

    [Back]Page Controller구현

--- src/main/java/com/yoonmin/board/controller/pageController.java
+++ src/main/java/com/yoonmin/board/controller/pageController.java
@@ -4,6 +4,7 @@ import com.yoonmin.board.domain.dto.PostDto;
 import com.yoonmin.board.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,19 +33,37 @@ public class pageController {
     public String openBoardWrite() throws Exception{
         return "/board/post";
     }
-    @RequestMapping(value = "/board/posts", method = RequestMethod.POST)
+    @RequestMapping(value = "/board", method = RequestMethod.POST, consumes ="application/json")
     public String insertBoard(PostDto postDto) throws Exception{
+        postService.savePost(postDto);
         return "redirect:/board";
     }
     //글 상세 화면
+    @RequestMapping(value ="/post/{post_id}", method = RequestMethod.GET)
+    public ModelAndView openBoardDetail(@PathVariable("post_id") Long boardId) throws Exception{
+        ModelAndView mv = new ModelAndView("/board/detail");
+        PostDto postDTO = postService.getPost(boardId);
+        mv.addObject("PostDto", postDTO);
+        return mv;
+    }
+
+    //글 수정 화면
+    @RequestMapping(value ="/post/edit/{post_id}", method = RequestMethod.GET)
+    public ModelAndView openBoardUpdate(@PathVariable("post_id") Long boardId) throws Exception {
+        ModelAndView mv = new ModelAndView("/board/update");
+        PostDto postDTO = postService.getPost(boardId);
+        mv.addObject("PostDto", postDTO);
+        return mv;
+    }
 
 
     //검색 화면
-    @RequestMapping(value ="/board/posts/{keyword}", method=RequestMethod.POST)
+    @RequestMapping(value ="/board/posts/{keyword}", method=RequestMethod.GET)
     public ModelAndView searchKeyword (@RequestParam(value="keyword") String keyword) throws Exception{
         ModelAndView mv = new ModelAndView("/board/home");
         List<PostDto> postDtoList = postService.searchPosts(keyword);
         mv.addObject("postList",postDtoList);
         return mv;
     }
+
 }
